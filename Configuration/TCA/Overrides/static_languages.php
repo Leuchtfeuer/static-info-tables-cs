278e794b123b049ecfd0b753ad80daf55080c002
tfeuerst <timo.feuerstein@gmx.de>
2020-02-07 11:26:39 +0100

    [TASK] Rename namespaces in TCA (#6)

--- Configuration/TCA/Overrides/static_languages.php
+++ Configuration/TCA/Overrides/static_languages.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') || die;
 
 call_user_func(
     function ($additionalFields, $dataSetName) {
-        \Bitmotion\StaticInfoTablesAr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
+        \Bitmotion\StaticInfoTablesCs\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
     },
     ['lg_name_en' => 'lg_name_cs'],
     'static_languages'
