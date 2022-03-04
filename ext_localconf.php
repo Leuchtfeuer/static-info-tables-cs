227793c7920321dc46ab5311e28c554d306834d2
Max Rösch <github@elbebass.com>
2022-03-04 12:37:20 +0100

    [TASK] Add editorconf and code style changes

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die;
 
 call_user_func(
-    function($extensionKey) {
+    function ($extensionKey) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
     }, \Bitmotion\StaticInfoTablesCs\Extension::EXTENSION_KEY
